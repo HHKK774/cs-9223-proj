60c40f52f368a27793ab13ffe1838f822af2d1de
MrMouse2019 <56179173+MrMouse2019@users.noreply.github.com>
2019-12-15 20:48:44 -0500

    Update upload_dynamodb.py

--- data_preprocessing/code/upload_dynamodb.py
+++ data_preprocessing/code/upload_dynamodb.py
@@ -17,15 +17,13 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('recipe')
 
 with open(file_path, newline='') as csvfile:
-    # reader1 = csv.reader(csvfile, delimiter=',')
-    # reader1 = csv.reader(csvfile, skipinitialspace=True)
     reader1 = csv.reader(csvfile, doublequote=True, quoting=csv.QUOTE_ALL, escapechar='\\')
     curr = 0
 
     for row in reader1:
         # if curr > 10:
         #     break
-        # print(row)
+
         temp = {}
 
         index = int(row[0])
@@ -39,67 +37,34 @@ with open(file_path, newline='') as csvfile:
 
         course_cuisine = row[3].strip("\"")
         course_cuisine = ast.literal_eval(course_cuisine)
-        # print(course_cuisine)
-        # print(type(course_cuisine))
         temp['course_cuisine'] = course_cuisine
 
-        # print(row[4])
         flavor = row[4].strip("\"")
-        # print(flavor)
         flavor = ast.literal_eval(flavor)
-        # print(type(flavor))
         temp['flavor'] = flavor
 
         small_image = row[5].strip("\"")
         small_image = ast.literal_eval(small_image)
-        # if len(small_image) != 1:
-        #     print(index)
-        # print(len(small_image))
-        # print(type(small_image))
-        # temp['small_image'] = small_image[small_image[0]]
+        temp['small_image'] = small_image['90']
+
         rating = int(row[6])
         temp['rating'] = rating
 
         big_image = row[7]
         temp['big_image'] = big_image
 
-        # print(row[8])
         ingredient_amount = ast.literal_eval(row[8])
-        # print(type(ingredient_amount))
         temp['ingredient_amount'] = ingredient_amount
 
-        # print(curr)
         curr += 1
 
         # print(temp)
 
-        # ddb_temp = json.loads(json.dumps(temp), parse_float=Decimal)
-        # response = table.put_item(
-        #     Item=ddb_temp
-        # )
+        ddb_temp = json.loads(json.dumps(temp), parse_float=Decimal)
+        response = table.put_item(
+            Item=ddb_temp
+        )
         # print(response)
-    print(curr)
-
-
-
-
-
-# df = pd.read_csv(file_path, sep=',', lineterminator='\n', error_bad_lines=False)
-# df = pd.read_csv(file_path, sep=',', lineterminator='\n')
-# print(len(df))
-# print(len(df.columns))
-# columns = list(df.columns)
-# print(columns)
-# sample = df.iloc[0]
-# print(sample['index'])
-# print(type(sample['index']))   # numpy.int64
-# print(sample['name'])
-# print(type(sample['name']))  # str
-# ingredient = ast.literal_eval(sample['ingredient'])
-# print(ingredient)
-# print(type(ingredient)) # list
-# course_cuisine = sample['course_cuisine']
-# print(course_cuisine)
-# print(type(course_cuisine))
-
-
+        if curr % 100 == 0:
+            print(curr)
+    # print(curr)
